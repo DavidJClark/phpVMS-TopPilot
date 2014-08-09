0031e9cd0f325cf21178fca6c45e390b599f76a4
DavidJClark <david@noticeabledesigns.com>
2014-08-08 20:37:15 -0400

    Updates for php 5.4 +
    Signed-off-by: DavidJClark <david@noticeabledesigns.com>

--- core/common/TopPilotData.class.php
+++ core/common/TopPilotData.class.php
@@ -11,56 +11,56 @@
 
 class TopPilotData extends CodonData
 {
-    public function get_monthly_stats($month, $year) {
+    public static function get_monthly_stats($month, $year) {
         $query = "SELECT * FROM ".TABLE_PREFIX."pireps WHERE MONTH(submitdate) = '$month' AND YEAR(submitdate) = '$year'";
 
         return DB::get_results($query);
     }
 
-    public function record_stats($pilot_id, $totalflights, $totaltime, $totalmiles, $startmonth, $startyear) {
+    public static function record_stats($pilot_id, $totalflights, $totaltime, $totalmiles, $startmonth, $startyear) {
         $query = "INSERT INTO ".TABLE_PREFIX."top_flights (pilot_id, flights, hours, miles, month, year)
                 VALUES('$pilot_id', '$totalflights', '$totaltime', '$totalmiles', '$startmonth', '$startyear')";
 
         DB::query($query);
     }
 
-    public function clear_table()   {
+    public static function clear_table()   {
         $query = "TRUNCATE TABLE ".TABLE_PREFIX."top_flights";
 
         DB::query($query);
     }
 
-    public function top_pilot_flights($month, $year, $howmany)    {
+    public static function top_pilot_flights($month, $year, $howmany)    {
         $query = "SELECT * FROM ".TABLE_PREFIX."top_flights WHERE month='$month' AND year='$year' ORDER BY flights DESC LIMIT $howmany";
 
         return DB::get_results($query);
     }
 
-    public function top_pilot_hours($month, $year, $howmany)    {
+    public static function top_pilot_hours($month, $year, $howmany)    {
         $query = "SELECT * FROM ".TABLE_PREFIX."top_flights WHERE month='$month' AND year='$year' ORDER BY hours DESC LIMIT $howmany";
 
         return DB::get_results($query);
     }
 
-    public function top_pilot_miles($month, $year, $howmany)    {
+    public static function top_pilot_miles($month, $year, $howmany)    {
         $query = "SELECT * FROM ".TABLE_PREFIX."top_flights WHERE month='$month' AND year='$year' ORDER BY miles DESC LIMIT $howmany";
 
         return DB::get_results($query);
     }
 
-    public function alltime_flights ($howmany)  {
+    public static function alltime_flights ($howmany)  {
         $query = "SELECT pilotid, totalflights FROM ".TABLE_PREFIX."pilots ORDER BY totalflights DESC LIMIT $howmany";
 
         return DB::get_results($query);
     }
 
-    public function alltime_hours ($howmany)  {
+    public static function alltime_hours ($howmany)  {
         $query = "SELECT pilotid, totalhours FROM ".TABLE_PREFIX."pilots ORDER BY totalhours DESC LIMIT $howmany";
 
         return DB::get_results($query);
     }
 
-    public function get_monthly_landingrate($month, $year, $howmany) {
+    public static function get_monthly_landingrate($month, $year, $howmany) {
         $query = "SELECT * FROM ".TABLE_PREFIX."pireps WHERE MONTH(submitdate) = '$month' AND YEAR(submitdate) = '$year' ORDER BY landingrate DESC LIMIT '$howmany'";
 
         return DB::get_results($query);
